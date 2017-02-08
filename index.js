8700af47b420493a918582b40316928207b5f60b
Guilherme Pacheco <guilherme.f.pacheco@gmail.com>
2017-02-08 01:28:16 -0200

    Add step by step game mode

--- index.js
+++ index.js
@@ -17,14 +17,30 @@ if (ais.length < 2) {
 }
 
 const game = new Game(ais);
+let playing = false;
 
+console.log('Game starting with these hands:');
 console.log(game.players.map((player) => player.toString()));
+console.log('Press any key to execute next move');
 
-game.step()
-  .then(() => {
-    console.log(game.state.previousPlays[0].toString());
-  })
-  .catch((err) => {
-    console.error(err);
-    process.exit(1);
-  });
+process.stdin.setRawMode(true);
+process.stdin.resume();
+process.stdin.setEncoding( 'utf8' );
+process.stdin.on('data', (key) => {
+  if (key === '\u0003') {
+    console.log('Game stopped');
+    process.exit();
+  }
+
+  if (!playing) {
+    game.step()
+      .then(() => {
+        console.log(game.state.previousPlays[game.state.previousPlays.length - 1].toString());
+        playing = false;
+      })
+      .catch((err) => {
+        console.error(err);
+        process.exit(1);
+      });
+  }
+});
